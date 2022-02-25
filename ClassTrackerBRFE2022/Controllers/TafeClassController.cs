10e4bb57162d395d988938c840c630d33bca5680
Shaun O'Sullivan <shaunosullivan90@live.com>
2022-02-25 10:06:12 +1000

    UI Extension examples

---
+++ ClassTrackerBRFE2022/Controllers/TafeClassController.cs
@@ -0,0 +1,91 @@
+﻿using ClassTrackerBRFE2022.Models.TafeClass;
+using ClassTrackerBRFE2022.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassTrackerBRFE2022.Controllers
+{
+    public class TafeClassController : Controller
+    {
+        // GET: TafeClassController
+        public ActionResult Index()
+        {
+            var tafeClasses = TafeClassService.GetAllTafeClasses();
+            return View(tafeClasses);
+        }
+
+        // GET: TafeClassController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TafeClassController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TafeClassController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TafeClassCreate tafeclass)
+        {
+            try
+            {
+                TafeClassService.CreateNewTafeClass(tafeclass);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TafeClassController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TafeClassController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TafeClassController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TafeClassController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ClassTrackerBRFE2022/Services/TafeClassService.cs
@@ -0,0 +1,95 @@
+﻿using ClassTrackerBRFE2022.Models.TafeClass;
+using ClassTrackerBRFE2022.Models.Teacher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ClassTrackerBRFE2022.Services
+{
+    public class TafeClassService
+    {
+        private static HttpClient _client;
+
+        private static void ConfigureClient()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:44379/api/");
+            _client.DefaultRequestHeaders.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static List<TafeClass> GetAllTafeClasses()
+        {
+            if (_client == null)
+            {
+                ConfigureClient();
+            }
+
+            HttpResponseMessage response = _client.GetAsync("TafeClass").Result;
+            List<TafeClass> tafeClasses = response.Content.ReadAsAsync<List<TafeClass>>().Result;
+
+            return tafeClasses;
+            //var result = response.Content.ReadAsStringAsync().Result;
+            //List<Teacher> teacherList = JsonSerializer.Deserialize<List<Teacher>>(result);
+        }
+
+        public static void CreateNewTafeClass(TafeClassCreate tafeClass)
+        {
+            if (_client == null)
+            {
+                ConfigureClient();
+            }
+
+            HttpResponseMessage response = _client.PostAsJsonAsync("TafeClass", tafeClass).Result;
+
+        }
+
+        // Get a single Teacher
+
+        public static TafeClass GetSingleTafeClass(int id)
+        {
+            // Requires an ID
+            if (_client == null)
+            {
+                ConfigureClient();
+            }
+
+            // Send a Get request to the specified endpoint (+ the ID!)
+            HttpResponseMessage response = _client.GetAsync($"TafeClass/{id}").Result;
+            // handle the response
+            var tafeClass = response.Content.ReadAsAsync<TafeClass>().Result;
+            // return a tafeClass
+            return tafeClass;
+        }
+
+        // Update a Teacher
+
+        public static void UpdateTafeClass(int id, TafeClass updatedTafeClass)
+        {
+            // requires an ID and a Teacher object
+            if (_client == null)
+            {
+                ConfigureClient();
+            }
+            // Send a Put request to the specified endpoint (+ the ID!)
+            HttpResponseMessage response = _client.PutAsJsonAsync($"TafeClass/{id}", updatedTafeClass).Result;
+            // Handle the response (check if success) 
+
+        }
+
+        // Delete a Teacher
+        public static void DeleteTafeClass(int id)
+        {
+            if (_client == null)
+            {
+                ConfigureClient();
+            }
+            // Send a Put request to the specified endpoint (+ the ID!)
+            HttpResponseMessage response = _client.DeleteAsync($"TafeClass/{id}").Result;
+        }
+
+    }
+}
